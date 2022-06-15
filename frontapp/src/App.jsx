04db93a47a8bf7f4004f0c5e182b5ce8e27edb2a
andersonsimplicio <andeson25eu@gmail.com>
2022-06-14 22:06:31 -0300

    First part project

---
+++ frontapp/src/App.jsx
@@ -0,0 +1,17 @@
+import Post from "./Post";
+
+
+export default function App() {
+    return (
+    <div>
+        <h1>REDE SOCIAL</h1>
+        <Post 
+        author="Anderson" 
+        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas rationeab vitae"
+        />
+    </div>
+  )
+}
+
+// author="Anderson" 
+// content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas rationeab vitae"
---
+++ frontapp/src/Post.jsx
@@ -0,0 +1,9 @@
+export default function Post(props){
+    return (
+        <div>
+            <h2>{props.author}</h2>
+            <p>{props.content}</p>
+        </div>
+         )
+            
+}
